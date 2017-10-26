19b86bec3c5d85eb645764c1eb7759d08429fc10
Katherine Espindola <yespindola@javeriana.edu.co>
2017-10-26 17:34:00 -0500

    accumulator fixed :)

--- logic/accounts_counter.py
+++ logic/accounts_counter.py
@@ -15,41 +15,30 @@ path = BBVA_Accounts_root.ROOT_DIR + os.path.sep + 'data' + os.path.sep
 F170404_file = open(path + "F170404.txt")
 F170404_list = F170404_file.read().splitlines()
 
+bbva_accumulator = {}
+
 unique_accounts = set()
-bbva_accounts = []
+
+count = 0
 
 for record in F170404_list:
     account = record[0:18]
     value_1 = int(record[19:33])
     value_2 = int(record[34:48])
+    if account in bbva_accumulator.keys():
+        old_value_1 = bbva_accumulator[account][0]
+        old_value_2 = bbva_accumulator[account][1]
+        new_value_1 = old_value_1 + value_1
+        new_value_2 = old_value_2 + value_2
+        bbva_accumulator[account] = [new_value_1, new_value_2]
+    else:
+        bbva_accumulator[account] = [value_1, value_2]
     unique_accounts.add(account)
-    bbva_accounts.append([account, value_1, value_2])
-
-p_file.write("Total unique accounts: " + str(len(unique_accounts)) + "\n")
-p_file.flush()
-execution_time = time() - start_time
-p_file.write("Read file execution time: " + str(timedelta(seconds=execution_time)) + "\n")
-p_file.flush()
-p_file.write("Accumulating values ... " + "\n")
-p_file.flush()
-
-bbva_accumulator = {}
-accumulator_value_1 = 0
-accumulator_value_2 = 0
-
-count = 0
-
-for account in unique_accounts:
-    for record in bbva_accounts:
-        if account == record[0]:
-            accumulator_value_1 += record[1]
-            accumulator_value_2 += record[2]
-    bbva_accumulator[account] = [accumulator_value_1, accumulator_value_2]
-    accumulator_value_1 = 0
-    accumulator_value_2 = 0
     count += 1
     print(count)
 
+p_file.write("Total unique accounts: " + str(len(unique_accounts)) + "\n")
+p_file.flush()
 execution_time = time() - start_time
 p_file.write("Accumulation values execution time: " + str(timedelta(seconds=execution_time)) + "\n")
 p_file.flush()
@@ -60,18 +49,27 @@ bad_accounts_file = open("BBVA_Accounts-BadAccountsResults.csv", 'a')
 
 count = 0
 
+good_accounts_count = 0
+bad_accounts_count = 0
+
 for account in bbva_accumulator:
     result_file.write(account + ";" + str(bbva_accumulator[account][0]) + ";" + str(bbva_accumulator[account][1]) + "\n")
     result_file.flush()
     if bbva_accumulator[account][0] == bbva_accumulator[account][1]:
         good_accounts_file.write(account + ";" + str(bbva_accumulator[account][0]) + ";" + str(bbva_accumulator[account][1]) + "\n")
         good_accounts_file.flush()
+        good_accounts_count += 1
     else:
         bad_accounts_file.write(account + ";" + str(bbva_accumulator[account][0]) + ";" + str(bbva_accumulator[account][1]) + "\n")
         bad_accounts_file.flush()
+        bad_accounts_count += 1
     count += 1
     print(count)
 
+p_file.write("Total good accounts: " + str(len(good_accounts_count)) + "\n")
+p_file.flush()
+p_file.write("Total bad accounts: " + str(len(bad_accounts_count)) + "\n")
+p_file.flush()
 execution_time = time() - start_time
 p_file.write("Results generation execution time: " + str(timedelta(seconds=execution_time)) + "\n")
 p_file.flush()
